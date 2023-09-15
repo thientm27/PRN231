ebcc42a368010402f58c399f8f9402a4c53e3faa
thientm27 <denkhotieu1@gmail.com>
2023-09-15 16:19:28 +0700

    Finish Lab1

--- Labs/Lab01_ASP.NETCoreWebAPI/ProductManagementApi/Controllers/ProductController.cs
+++ Labs/Lab01_ASP.NETCoreWebAPI/ProductManagementApi/Controllers/ProductController.cs
@@ -1,49 +1,72 @@
-// using BusinessObjects;
-// using Microsoft.AspNetCore.Mvc;
-// using Repositories;
-//
-// namespace ProductManagementApi.Controllers;
-//
-// [ApiController]
-// [Route("[controller]")]
-// public class ProductController : ControllerBase
-// {
-//     private IProductRepository _productRepository = new ProductRepository();
-//
-//     [HttpGet]
-//     public ActionResult<IEnumerable<Products>> GetProducts()
-//     {
-//         return _productRepository.GetProducts();
-//     }
-//     
-//     [HttpPost]
-//     public IActionResult PostProduct(Products p)
-//     {
-//          _productRepository.SaveProduct(p);
-//          return NotFound();
-//     }
-//     
-//     [HttpDelete("id")]
-//     public IActionResult DeleteProduct(int id)
-//     {
-//         var product = _productRepository.GetProductsById(id);
-//         if (product == null)
-//         {
-//             return NotFound();
-//         }
-//         _productRepository.DeleteProduct(product);
-//         return NotFound();
-//     }
-//       
-//     [HttpPut("id")]
-//     public IActionResult UpdateProduct(int id, Products p)
-//     {
-//         var product = _productRepository.GetProductsById(id);
-//         if (product == null)
-//         {
-//             return NotFound();
-//         }
-//         _productRepository.UpdateProduct(p);
-//         return NotFound();
-//     }
-// }
+using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Repositories;
+
+namespace ProductManagementApi.Controllers;
+
+[Route("api/product/[action]")]
+[ApiController]
+public class ProductsControllers : ControllerBase
+{
+    private IProductRepository repository = new ProductRepository();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetProducts() => repository.GetProducts();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Category>> GetCategories() => repository.GetCategories();
+
+    [HttpGet("id")]
+    public ActionResult<Product> GetProductById(int id) => repository.GetProductById(id);
+
+    [HttpPost]
+    public IActionResult PostProduct(Product p)
+    {
+        var product = repository.GetProductById(p.ProductId);
+        if (p == null)
+        {
+            return NoContent();
+        }
+        repository.SaveProduct(p);
+        return NoContent();
+    }
+
+    [HttpDelete("id")]
+    public IActionResult DeleteProduct(int id)
+    {
+        var p = repository.GetProductById(id);
+        if (p == null)
+        {
+            return NotFound();
+        }
+
+        repository.DeleteProduct(p);
+        return NoContent();
+    }
+
+    [HttpPut("id")]
+    public IActionResult UpdateProduct(int id, Product p)
+    {
+        var pTmp = repository.GetProductById(id);
+        if (p == null)
+        {
+            return NotFound();
+        }
+
+        repository.UpdateProduct(p);
+        return NoContent();
+    }
+}
+
+[Route("api/pro")]
+[ApiController]
+public class ProControllers : ControllerBase
+{
+    private IProductRepository repository = new ProductRepository();
+
+    [HttpGet("get-product-by-id/{id}/detail")]
+    public ActionResult<Product> GetPById(int id)
+    {
+        return repository.GetProductById(id);
+    }
+}
---
+++ Labs/Lab01_ASP.NETCoreWebAPI/ProductManagementWebClient/Controllers/ProductController.cs
@@ -0,0 +1,133 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace ProductManagementWebClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string ProductApiUrl = "";
+
+        public ProductController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            ProductApiUrl = "https://localhost:7028/api/product";
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await client.GetAsync(ProductApiUrl + "/GetProducts");
+            List<Product>? products = new List<Product>();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                products = response.Content.ReadFromJsonAsync<List<Product>>().Result;
+            }
+
+			HttpResponseMessage response2 = await client.GetAsync(ProductApiUrl + "/GetCategories");
+			List<Category>? categories = new List<Category>();
+			if (response2.StatusCode == System.Net.HttpStatusCode.OK)
+			{
+				categories = response2.Content.ReadFromJsonAsync<List<Category>>().Result;
+			}
+			ViewData["CategoryId"] = new SelectList(categories, "CategoryId", "CategoryName");
+
+			return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            HttpResponseMessage response = await client.GetAsync(ProductApiUrl + "/GetProductById/id?id=" + id);
+
+            Product product = new Product();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                product = response.Content.ReadFromJsonAsync<Product>().Result;
+            }
+
+            HttpResponseMessage response2 = await client.GetAsync(ProductApiUrl + "/GetCategories");
+            List<Category>? categories = new List<Category>();
+            if (response2.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                categories = response2.Content.ReadFromJsonAsync<List<Category>>().Result;
+            }
+            ViewData["category"] = categories;
+            return View(product);
+        }
+
+        //GET: ProductsController/Create
+        public async Task<IActionResult> Create()
+        {
+            HttpResponseMessage response = await client.GetAsync(ProductApiUrl + "/GetCategories");
+            List<Category>? categories = new List<Category>();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                categories = response.Content.ReadFromJsonAsync<List<Category>>().Result;
+            }
+            ViewData["category"] = categories;
+            return View();
+        }
+
+        // GET: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = await client.PostAsJsonAsync(ProductApiUrl + "/PostProduct", p);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return Redirect("Create");
+        }
+
+        // GET: ProductController/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            HttpResponseMessage response = await client.GetAsync(ProductApiUrl + "/GetProductById/id?id=" + id);
+            Product product = new Product();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                product = response.Content.ReadFromJsonAsync<Product>().Result;
+            }
+
+            HttpResponseMessage response2 = await client.GetAsync(ProductApiUrl + "/GetCategories");
+            List<Category>? categories = new List<Category>();
+            if (response2.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                categories = response2.Content.ReadFromJsonAsync<List<Category>>().Result;
+            }
+            ViewData["category"] = categories;
+            return View(product);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                await client.PutAsJsonAsync(ProductApiUrl + "/UpdateProduct/id?id=" + id, p);
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        // GET: ProductController/Delete.5
+        public async Task<IActionResult> Delete(int id)
+        {
+            await client.DeleteAsync(ProductApiUrl + "/DeleteProduct/id?id=" + id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
