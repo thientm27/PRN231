ec55e0f2a3ab5572976156e3ae481aec3871f451
thientm27 <denkhotieu1@gmail.com>
2023-10-04 21:08:31 +0700

    lab 2 client view

---
+++ Labs/Lab02_BookStoreOData8/BookStore.Client/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.ODataApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace ODataBookStoreWebClient.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string BookApiUrl = "";
+
+        public BookController()
+        {
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            BookApiUrl = "http://localhost:5138/odata/Books";
+        }
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage res = await client.GetAsync(BookApiUrl);
+            string strData = await res.Content.ReadAsStringAsync();
+
+            dynamic temp = JObject.Parse(strData);
+            var lst = temp.value;
+            List<Book> items = ((JArray)temp.value).Select(x => new Book
+            {
+                Id = (int)x["Id"],
+                Author = (string)x["Author"],
+                ISBN = (string)x["ISBN"],
+                Title = (string)x["Title"],
+                Price = (decimal)x["Price"]
+            }).ToList();
+
+
+            return View(items);
+        }
+        
+    }
+}
